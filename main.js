e2ec3b3a793ca90a16da115ff6c2227f77321c93
Greg Anderson <greg@infobytes.com>
2014-05-17 15:49:43 -0600

    Added a timer that counts down to zero, refactored code as well

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
 var app = angular.module('clockOut', []);
 
-app.controller("Test", function($scope) {
-	$scope.testFunction = function () {
-		alert("Hey it worked!");
-	}
-})
 
-app.directive("test", function() {
-	return function(scope, element, attrs) {
-		element.bind("click", function() {
-			scope.$apply(attrs.test);
-		})
+app.controller("Timer", function ($scope,$timeout) {
+	$scope.time = 0;
+    $scope.counter = 0;
+	$scope.started = false;
+
+    $scope.onTimeout = function() {
+        $scope.counter -= .01;
+		if ($scope.counter.toFixed(2) == 0) {
+			$timeout.cancel(mytimeout);
+			console.log("timer expired");
+			alert("done");
+		}
+		else
+			mytimeout = $timeout($scope.onTimeout,10);
+    }
+
+	$scope.start = function(time) {
+		// Prevent user from starting more than once
+		if ($scope.started)
+			return;
+		$scope.started = true;
+		$scope.counter = (40.0 - time) * 60;
+		var mytimeout = $timeout($scope.onTimeout,10);
 	}
 })
