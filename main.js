9db6f3e507a56e9b35f161b4b5d880306e2d715f
Greg Anderson <greg@infobytes.com>
2014-05-17 22:36:18 -0600

    Added functionality from @unaveed
    Now able to calculate what time the user should clock out.

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-var app = angular.module('clockOut', []);
+var clockoutApp = angular.module('clockOut', []);
 
-
-app.controller("Timer", function ($scope,$timeout) {
+clockoutApp.controller('ClockCtrl', function ($scope,$timeout) {
 	$scope.time = 0;
-	$scope.hours = 0;
-	$scope.minutes = 0;
-	$scope.seconds = 0;
+	$scope.hoursRemaining = 0;
+	$scope.minutesRemaining = 0;
+	$scope.secondsRemaining = 0;
     $scope.counter = 0;
 	$scope.started = false;
+	$scope.d = Date.now();
 
     $scope.onTimeout = function() {
         $scope.counter -= .01;
 		var total = $scope.counter / 60 / 60;
-		$scope.hours = parseInt(total);
-		total = (total - $scope.hours) * 60;
-		$scope.minutes = parseInt(total);
-		total = (total - $scope.minutes) * 60;
-		$scope.seconds = total.toFixed(2);
+		$scope.hoursRemaining = parseInt(total);
+		total = (total - $scope.hoursRemaining) * 60;
+		$scope.minutesRemaining = parseInt(total);
+		total = (total - $scope.minutesRemaining) * 60;
+		$scope.secondsRemaining = total.toFixed(2);
 
 		if ($scope.counter.toFixed(2) == 300) {
 			document.getElementById("five_minutes").play();
@@ -42,4 +42,77 @@ app.controller("Timer", function ($scope,$timeout) {
 		$scope.counter = (40.0 - time) * 60 * 60 - 30;
 		var mytimeout = $timeout($scope.onTimeout,10);
 	}
-})
+
+
+
+
+
+	/**************************** UMAIR START ************************/
+
+
+	$scope.calculate = function (_timeIn, _hours) {
+		if(_timeIn, _hours) {
+			var timeArray, hour, minutes, totalTime, timeLeft, hoursWorked; 
+			
+			timeLeft = 40 - _hours;
+			
+			timeArray = _timeIn.split(":");
+			
+			hour = timeArray[0];
+			minutes = timeArray[1];
+			
+			$scope.message = 'You should clock out at: ' + calculateTime(timeLeft, hour, minutes) + 'PM';
+		}
+	};
+	
+	var calculateTime = function(timeLeft, hour, minutes) {
+		
+		var check = timeLeft.toString().indexOf(".");
+
+		if(check === -1) {	
+			hour += timeLeft;
+			hour = hour % 12;
+
+			if (hour == 0)
+				hour = 12;
+			
+			return (hour + ':' + minutes);
+		}
+		else {
+			var accumulatedTimeArray, hrs, tempMin,  min; 
+
+			accumulatedTimeArray = timeLeft.toString().split('.');
+			
+			hrs = (accumulatedTimeArray[0]);
+			tempMin = accumulatedTimeArray[1];
+
+			tempMin = "0." + tempMin;
+
+			min = parseFloat(tempMin);
+
+			min = (min * 60);
+			
+			hour = +hour + +hrs; 
+			minutes = (+minutes + +min);
+			
+			if(minutes > 59) {
+				hour++;
+				minutes = 60 - +minutes;
+			}
+
+			if(minutes < 10)
+				minutes = "0" + minutes;
+			
+			hour = hour % 12;
+			if (hour == 0)
+				hour = 12;
+
+			return (hour + ':' + parseInt(minutes)); 
+		}
+	
+	};
+	
+	$scope.display = function() {
+		return this.message;	
+	};
+});
